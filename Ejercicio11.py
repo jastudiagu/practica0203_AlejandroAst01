d180fb1a8549324b93ab3f85435597afe3b1b4b8
Alejandro Astu <jastudiagu@educacion.navarra.es>
2023-11-02 23:49:28 +0100

    Ej 11

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,4 +1,11 @@
 a = [[1, 2, 3], [4, 5, 6]]
 b = [[-1, 0], [0, 1], [1, 1]]
+r = [[0, 0], [0, 0]]
 
-print( a * b)
+
+for i in range(len(a)):
+    for j in range(len(b[0])):
+        for k in range(len(b)):
+            r[i][j] = r[i][j] + a[i][k] * b[k][j]
+
+print(r)
